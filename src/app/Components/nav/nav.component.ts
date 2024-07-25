66bcc47d8f8436a11e3d910729856abf9be7fcfc
MarkNicholas-cpp <marknicholas2002112@gmail.com>
2024-07-25 21:54:56 +0530

    Simple changes V -01 Perfect Deployed

--- src/app/Components/nav/nav.component.ts
+++ src/app/Components/nav/nav.component.ts
@@ -1,20 +1,42 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   isScrolled = false;
-  isNavOpen = false; 
+  isNavOpen = false;
+  isMobile = false;
+
+  ngOnInit() {
+    this.checkScreenWidth();
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 0;
   }
 
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenWidth();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const targetElement = event.target as HTMLElement;
+    if (this.isNavOpen && this.isMobile && !targetElement.closest('.nav-bar')) {
+      this.isNavOpen = false;
+    }
+  }
+
   responsiveNav() {
-    this.isNavOpen = !this.isNavOpen; 
+    this.isNavOpen = !this.isNavOpen;
+  }
+
+  checkScreenWidth() {
+    this.isMobile = window.innerWidth <= 768;
   }
 }
